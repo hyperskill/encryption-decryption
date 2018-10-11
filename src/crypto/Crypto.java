49d4dc43af7c1411d6726d4b6cd0326f08e095be
art <swsms@mail.ru>
2018-10-11 22:52:38 +0500

    Add the basic project's structure

---
+++ src/crypto/Crypto.java
@@ -0,0 +1,10 @@
+package crypto;
+
+import java.util.Scanner;
+
+public class Crypto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+	// write your code here
+    }
+}

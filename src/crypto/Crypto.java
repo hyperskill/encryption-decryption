f96ee4d6e5c496262decdfc1efd735462c59b220
JoraTracktor <39220306+JoraTracktor@users.noreply.github.com>
2018-11-15 16:05:01 +0300

    Update Crypto.java

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 public class Crypto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-	// write your code here
+	
+	String act = scanner.nextLine();
+        String line = scanner.nextLine();
+        int shift = scanner.nextInt();  
+        String newLine = "";
+        
+        switch (act) {
+        case "enc":
+        	for (int i = 0; i < line.length(); i++) {  	
+      			newLine += (char)  (line.charAt(i) + shift);
+        	}
+        	System.out.print(newLine);  
+        	break;
+        	
+        case "dec":
+        	for (int i = 0; i < line.length(); i++) {  	
+      			newLine += (char)  (line.charAt(i) - shift);
+            	}
+        	System.out.print(newLine);  
+        	break;
+        default:
+        	System.out.println("Unknown operation");
+        }
     }
 }

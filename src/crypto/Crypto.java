8d06113c1e38e5804a954d11387e00827af7cfca
IAxesus <45329580+IAxesus@users.noreply.github.com>
2018-12-04 20:18:38 +0400

    Update Crypto.java

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -3,8 +3,45 @@ package crypto;
 import java.util.Scanner;
 
 public class Crypto {
+
+    private static Scanner scanner;
+    private static char[] array;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-	// write your code here
+        String value = scanner.nextLine();
+        String string = scanner.nextLine();
+        int key = scanner.nextInt();
+
+        if(value.equals("enc")) {
+            encryption(string, key);
+        } else if(value.equals("des")) {
+            descryption(string, key);
+        }
+    }
+
+    private static void descryption(String string, int key) {
+        char[] arrayString = string.toCharArray();
+        array = new char[string.length()];
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = (char) (arrayString[i] - key);
+        }
+
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+    }
+
+    private static void encryption(String string, int key) {
+        char[] arrayString = string.toCharArray();
+        array = new char[string.length()];
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = (char) (arrayString[i] + key);
+        }
+
+        for(int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
     }
 }

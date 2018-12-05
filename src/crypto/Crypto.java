7882ed58d8e26447ac7bbb53835161427e0721bb
kozhanov <kosev@mail.ru>
2018-12-05 15:02:23 +0400

    Encode & Decode command prompt and files

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,9 +1,15 @@
 package crypto;
 
-public class Crypto {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Crypto  {
+    public static void main(String[] args) throws IOException {
         String mode = "";
-        String text = "";
+        String filein = "";
+        String fileout = "";
         int key = 0;
         for (int i = 0; i < args.length; i++) {
             switch (args[i])
@@ -16,26 +22,38 @@ public class Crypto {
                     if (i+1 < args.length)
                         key = Integer.parseInt(args[i+1]);
                     break;
-                case "-data":
+                case "-in":
                     if (i+1 < args.length)
-                        text = args[i+1];
+                        filein = args[i+1];
+                    break;
+                case "-out":
+                    if (i+1 < args.length)
+                        fileout = args[i+1];
                     break;
             }
         }
 
-        if (mode.isEmpty()||text.isEmpty()) return;
+        if (mode.isEmpty()||filein.isEmpty()||fileout.isEmpty()||!new File(filein).exists()) return;
 
-        System.out.println(encode(mode, text, key));
+        FileInputStream fi = new FileInputStream(filein);
+        FileOutputStream fo = new FileOutputStream(fileout);
+
+        while (fi.available() > 0 )
+        {
+            int data = fi.read();
+            fo.write(data+ (mode.equals("enc")?key:mode.equals("dec")?key*-1:0));
+        }
+
     }
 
-    private static String encode(String mode, String text, int key)
+    /*private static String encode(String mode, String text, int key)
     {
         String enctext = "";
         for (char c : text.toCharArray()) {
             enctext += (char)((int)c+ (mode.equals("enc")?key:mode.equals("dec")?key*-1:0));
         }
         return enctext;
-    }
+    }*/
 
 
 }

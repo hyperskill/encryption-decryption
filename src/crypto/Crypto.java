9af0e341e4bcbbc455bc7df4af3c54c1d77bde09
kozhanov <kosev@mail.ru>
2018-12-05 11:45:38 +0400

    Encode & Decode

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -5,6 +5,34 @@ import java.util.Scanner;
 public class Crypto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-	// write your code here
+        String mode = scanner.nextLine();
+        String text = scanner.nextLine();
+        int key = Integer.parseInt(scanner.nextLine());
+        String alph = "abcdefghijklmnopqrstuvwxyz";
+        String enctext = "";
+        switch (mode) {
+            case "enc":
+                for (char c : text.toLowerCase().toCharArray()) {
+                    int i = alph.indexOf(c);
+                    if (i != -1) {
+                        enctext += alph.charAt(i + key < alph.length() ? i + key : i + key - alph.length());
+                    } else {
+                        enctext += c;
+                    }
+                }
+                break;
+            case "dec":
+                for (char c : text.toLowerCase().toCharArray()) {
+                    int i = alph.indexOf(c);
+                    if (i != -1) {
+                        enctext += alph.charAt(i - key > 0 ? i - key : i - key + alph.length());
+                    } else {
+                        enctext += c;
+                    }
+                }
+                break;
+        }
+        System.out.println(enctext);
+
     }
 }

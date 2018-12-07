016760d208ede63859c5b1ccda44a4c0d5b892cb
kozhanov <kosev@mail.ru>
2018-12-07 15:37:30 +0400

    Encode & Decode command prompt and files & alg

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -5,12 +5,68 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+interface ICrypto
+{
+    byte[] encrypt(byte[] data, String key);
+    byte[] decrypt(byte[] data, String key);
+}
+
+class CryptoShift implements ICrypto
+{
+    @Override
+    public byte[] encrypt(byte[] data, String key) {
+
+        String text = "";
+        for (int i = 0; i < data.length; i++) {
+            text += (char)(data[i] + Integer.parseInt(key));
+        }
+        return text.getBytes();
+    }
+    @Override
+    public byte[] decrypt(byte[] data, String key) {
+
+        String text = "";
+        for (int i = 0; i < data.length; i++) {
+            text += (char)(data[i] - Integer.parseInt(key));
+        }
+        return text.getBytes();
+    }
+}
+
+class Crypter
+{
+    private ICrypto alg;
+
+    public Crypter(String alg) throws Exception
+    {
+        switch (alg.toLowerCase()) {
+            case "shift":
+                this.alg = new CryptoShift();
+                break;
+            default:
+                throw new Exception("Invalid algorithm");
+        }
+    }
+
+    public byte[] crypt(byte[] data, String key, String mode)
+    {
+        if (mode.toLowerCase().equals("enc"))
+            return alg.encrypt(data, key);
+        if (mode.toLowerCase().equals("dec"))
+            return alg.decrypt(data, key);
+        return null;
+    }
+
+}
+
+
 public class Crypto  {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
         String mode = "";
         String filein = "";
         String fileout = "";
-        int key = 0;
+        String alg = "";
+        String key = "";
         for (int i = 0; i < args.length; i++) {
             switch (args[i])
             {
@@ -20,7 +76,7 @@ public class Crypto  {
                     break;
                 case "-key":
                     if (i+1 < args.length)
-                        key = Integer.parseInt(args[i+1]);
+                        key = args[i+1];
                     break;
                 case "-in":
                     if (i+1 < args.length)
@@ -30,6 +86,10 @@ public class Crypto  {
                     if (i+1 < args.length)
                         fileout = args[i+1];
                     break;
+                case "-alg":
+                    if (i+1 < args.length)
+                        alg = args[i+1];
+                    break;
             }
         }
 
@@ -38,22 +98,14 @@ public class Crypto  {
         FileInputStream fi = new FileInputStream(filein);
         FileOutputStream fo = new FileOutputStream(fileout);
 
-        while (fi.available() > 0 )
-        {
-            int data = fi.read();
-            fo.write(data+ (mode.equals("enc")?key:mode.equals("dec")?key*-1:0));
-        }
+        byte[] data = new byte[fi.available()];
+        fi.read(data,0,fi.available());
+        Crypter cr = new Crypter(alg);
+        byte[] cryptodata = cr.crypt(data, key, mode);
+        if (cryptodata == null) System.out.println("Invalid mode");
+        fo.write(cryptodata);
+
 
     }
-
-    /*private static String encode(String mode, String text, int key)
-    {
-        String enctext = "";
-        for (char c : text.toCharArray()) {
-            enctext += (char)((int)c+ (mode.equals("enc")?key:mode.equals("dec")?key*-1:0));
-        }
-        return enctext;
-    }*/
-
-
 }
+

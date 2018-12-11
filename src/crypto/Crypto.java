6e35c5c7a574a4f512b63dda57a244828ca7d374
AIexMoran <homin.sashawork@gmail.com>
2018-12-11 23:57:20 +0300

    first version

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -3,8 +3,44 @@ package crypto;
 import java.util.Scanner;
 
 public class Crypto {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-	// write your code here
+        Crypto m = new Crypto();
+        m.start();
+    }
+
+    public void start() {
+        Scanner scn = new Scanner(System.in);
+        String command = scn.nextLine();
+        String message;
+        int key;
+        switch (command) {
+            case "encrypt":
+                message = scn.nextLine();
+                key = scn.nextInt();
+                System.out.println(encrypt(message, key));
+                break;
+            case "decrypt":
+                message = scn.nextLine();
+                key = scn.nextInt();
+                System.out.println(decrypt(message, key));
+                break;
+        }
+    }
+
+    public String encrypt(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            result.append((char) (message.charAt(i) + key));
+        }
+        return result.toString();
+    }
+
+    public String decrypt(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            result.append((char) (message.charAt(i) - key));
+        }
+        return result.toString();
     }
 }

e5bb06a9078f12f6c190462b16c6ff3ea8b94a1b
Ilya <ilia.bogatyrve>
2018-12-12 18:03:28 +0300

    Stage4 completed

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,17 +1,37 @@
 package crypto;
 
-import java.util.Scanner;
+import java.util.*;
 
 import translater.Translater;
 import translater.TranslaterFactory;
 
 public class Crypto {
     public static void main(String[] args) {
-    	Scanner scanner = new Scanner(System.in);
-    	String cmd = scanner.nextLine();
-    	String message = scanner.nextLine();
-    	int key = scanner.nextInt();
-    	scanner.close();
+    	String cmd = new String("");
+    	int key = 0;
+    	String message = new String("");
+    	boolean check = false;
+    	for (int i = 0; i < args.length; i++) {
+    		if ("-mode".equals(args[i])) {
+    			cmd = args[++i];
+    			continue;
+    		}
+    		if ("-key".equals(args[i])) {
+    			key = Integer.parseInt(args[++i]);
+    			continue;
+    		}
+    		if ("-data".equals(args[i])) {
+    			message = args[++i];
+    			check = true;
+    			continue;
+    		}
+    	}
+    	if (!check) {
+    		Scanner scanner = new Scanner(System.in);
+    		message = scanner.next();
+    		key = scanner.nextInt();
+    		scanner.close();    		
+    	}
     	Translater translater = TranslaterFactory.getTranslater(cmd,message,key);
     	System.out.println(translater.translate());
     }

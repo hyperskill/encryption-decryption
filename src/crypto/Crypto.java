1a2d05559c676662bf880945f3ed913de526cc3d
Nikita Karpov <bonilka@gmail.com>
2018-12-13 22:52:31 +0300

    -add enc; -add dec;

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -5,6 +5,39 @@ import java.util.Scanner;
 public class Crypto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-	// write your code here
+
+        String target = scanner.nextLine();
+//        String message = "Wabc";
+        String message = scanner.nextLine();
+        int offset = scanner.nextInt();
+
+        if(target.equals("enc")){
+            System.out.println(enc(message, offset));
+        } else if(target.equals("dec")){
+            System.out.println(dec(message, offset));
+        }
+
+    }
+    public static String enc(String originalStr, int offset){
+        return offsetAll(originalStr, offset);
+    }
+
+    public static String dec(String originalStr, int offset){
+        return offsetAll(originalStr, -offset);
+    }
+    public static String offsetAll(String originalStr, int offset){
+        final int lettersStar = 97;
+        final int lettersAmount = 26;
+
+        StringBuilder out = new StringBuilder();
+        char[] letters = new char[originalStr.length()];
+        originalStr.getChars(0, originalStr.length(), letters, 0  );
+
+        for (int letter : letters){
+            letter = (letter + offset);
+            out.append((char)letter);
+        }
+
+        return out.toString();
     }
 }

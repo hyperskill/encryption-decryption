47740bf61191c18f1b88b01cc5f00cdf1280ce1a
AIexMoran <homin.sashawork@gmail.com>
2018-12-23 14:25:40 +0300

    added "-out" "-in"

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,9 +1,13 @@
 package crypto;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Crypto {
+
+    private Scanner scn;
+    private FileWriter fw;
 
     public static void main(String[] args) {
         Crypto m = new Crypto();
@@ -11,40 +15,54 @@ public class Crypto {
     }
 
     public void startWithArgs(String[] args) {
-        if (args.length == 6) {
-            if (args[0].equals("-mode") && args[2].equals("-key") && args[4].equals("-data")) {
-                switch (args[1]) {
-                    case "dec":
-                        System.out.println(decrypt(args[5], Integer.parseInt(args[3])));
-                        break;
-                    case "enc":
-                        System.out.println(encrypt(args[5], Integer.parseInt(args[3])));
-                        break;
+        if (args.length < 6) {
+            return;
+        }
+        String mode = "";
+        String data = "";
+        int key = 0;
+        if (!args[0].equals("-mode")) {
+            System.out.println("Bad args");
+            return;
+        } else {
+            mode = args[1];
+        }
+        if (!args[2].equals("-key")) {
+            System.out.println("Bad args");
+            return;
+        } else {
+            key = Integer.parseInt(args[3]);
+        }
+        if (args[4].equals("-data")) {
+            data = args[5];
+        } else if (args[4].equals("-in")) {
+            try {
+                scn = new Scanner(new File(args[5]));
+                while (scn.hasNextLine()) {
+                    data += scn.nextLine() + "\n";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            scn = new Scanner(System.in);
+            data = scn.nextLine();
+        }
+        if (args.length > 6) {
+            if (args[6].equals("-out")) {
+                try {
+                    fw = new FileWriter(new File(args[7]));
+                    fw.write(data);
+                    fw.flush();
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         } else {
-            System.out.println("bad args");
-            return;
+            System.out.println(data);
         }
-    }
 
-    public void start() {
-        Scanner scn = new Scanner(System.in);
-        String command = scn.nextLine();
-        String message;
-        int key;
-        switch (command) {
-            case "encrypt":
-                message = scn.nextLine();
-                key = scn.nextInt();
-                System.out.println(encrypt(message, key));
-                break;
-            case "decrypt":
-                message = scn.nextLine();
-                key = scn.nextInt();
-                System.out.println(decrypt(message, key));
-                break;
-        }
     }
 
     public String encrypt(String message, int key) {

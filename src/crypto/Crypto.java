cdf22d5196e9db80f93d48e9ccc7c1f8d3ac4f66
Tanya Tarinskaya <36923363+tantarin@users.noreply.github.com>
2018-12-27 14:04:25 +0300

    Update Crypto.java

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,10 +1,19 @@
-package crypto;
-
-import java.util.Scanner;
-
 public class Crypto {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-	// write your code here
+        String mode ="dec";
+        char[] dataText = args[5].toCharArray();
+        int keyNumber = Integer.parseInt(args[3]);
+        if (args[1].equals(mode)){
+            for (int x = 0; x < dataText.length; x = x + 1) {
+	            int y = (int)dataText[x]-keyNumber;
+                System.out.print((char)y);
+	        } 
+        }   
+        else {
+            for (int x = 0; x < dataText.length; x = x + 1) {
+	            int y = (int)dataText[x]+keyNumber;
+                System.out.print((char)y);
+            }
+        }
     }
 }

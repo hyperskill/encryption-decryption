4f674a1bc0207a9acbef327927b410ec8bf25685
Sensdoo <sensibleprogrammer@gmail.com>
2018-12-28 23:46:14 +0300

    add command line args

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -5,11 +5,37 @@ import java.util.Scanner;
 public class Crypto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String state = scanner.nextLine();
-        String message = scanner.nextLine();
-        int key = scanner.nextInt();
+        String state = "";
+        String message = "";
+        int key = 0;
         String result = "";
+
+        for (int i = 0; i < args.length; i++) {
+            if ("-mode".equals(args[i])) {
+                state = args[i + 1];
+                i++;
+            } else if ("-key".equals(args[i])) {
+                key = Integer.parseInt(args[i + 1]);
+                i++;
+            } else if ("-data".equals(args[i])) {
+                message = args[i + 1];
+                i++;
+            }
+        }
+
+        if ("".equals(message)) {
+            System.out.print("Enter text: ");
+            message = scanner.nextLine();
+        }
+
+        if ("".equals(state)) {
+            state = "enc";
+        }
+
+        if (key == 0) {
+            System.out.print("Enter key: ");
+            key = scanner.nextInt();
+        }
 
         if ("enc".equals(state)) {
             result = encryption(message, key);

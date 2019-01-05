ff8b6c99e5c3d1f20c5971e838894d87d1dcd1f1
Sensdoo <sensibleprogrammer@gmail.com>
2019-01-05 13:45:04 +0300

    add work with files

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,14 +1,21 @@
 package crypto;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Crypto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String state = "";
+        String state = "enc";
         String message = "";
         int key = 0;
         String result = "";
+        String in = "";
+        String out = "";
 
         for (int i = 0; i < args.length; i++) {
             if ("-mode".equals(args[i])) {
@@ -20,21 +27,34 @@ public class Crypto {
             } else if ("-data".equals(args[i])) {
                 message = args[i + 1];
                 i++;
+            } else if ("-in".equals(args[i])) {
+                in = args[i + 1];
+                i++;
+            } else if ("-out".equals(args[i])) {
+                out = args[i + 1];
+                i++;
             }
-        }
-
-        if ("".equals(message)) {
-            System.out.print("Enter text: ");
-            message = scanner.nextLine();
-        }
-
-        if ("".equals(state)) {
-            state = "enc";
         }
 
         if (key == 0) {
             System.out.print("Enter key: ");
             key = scanner.nextInt();
+        }
+
+        if (!in.equals("")) {
+            File input = new File(in);
+            if (input.exists()) {
+                try {
+                    message = new String(Files.readAllBytes(Paths.get(input.getPath())));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if (message.equals("")) {
+            System.out.print("Enter text: ");
+            message = scanner.nextLine();
         }
 
         if ("enc".equals(state)) {
@@ -45,7 +65,30 @@ public class Crypto {
             result = decryption(message, key);
         }
 
-        System.out.println(result);
+        if (out.equals("")) {
+            System.out.println(result);
+        } else {
+            File output = new File(out);
+            boolean created = true;
+            if (!output.exists()) {
+                try {
+                    created = output.createNewFile();
+                } catch (IOException e) {
+                    created = false;
+                    System.out.println("Cannot create the file: " + output.getPath());
+                }
+            }
+            if (created) {
+                try {
+                    FileWriter writer = new FileWriter(output);
+                    writer.write(result);
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     public static String encryption(String message, int key) {

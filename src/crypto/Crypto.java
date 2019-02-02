d71a377442470ddee113c370281478c9f570335a
Kirill Kornakov <KirillKornakov@narod.ru>
2019-02-02 11:53:15 +0300

    Stage #4: Using command-line arguments

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -26,13 +26,23 @@ public class Crypto {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String target = scan.nextLine();
-        String message = scan.nextLine();
-        int key = scan.nextInt();
-        if (target.equals("enc")) {
-            encryption(message, key);
+        String message, target;
+        int key;
+        int indTarget=-1, indMessage=-1, indKey=-1;
+        for (int i=0; i<args.length-1; i++){
+            if(args[i].equals("-mode")) indTarget=i;
+            if(args[i].equals("-key")) indKey=i;
+            if(args[i].equals("-data")) {indMessage=i; break;}
         }
-        else if (target.equals("dec")) decryption(message, key);
+
+        if(indTarget>=0) target=args[indTarget+1];
+        else target="enc";
+        if (indKey>=0) key=Integer.valueOf(args[indKey+1]);
+        else key = scan.nextInt();
+        if (indMessage>=0) message=args[indMessage+1];
+        else message = scan.nextLine();
+        if (target.equals("enc") || target.equals("")) encryption(message, key);
+        else decryption(message, key);
 
     }
 }

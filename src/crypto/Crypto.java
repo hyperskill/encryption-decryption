c9f56b2b5feaaa64f3f41b496959c3b442d606df
ShilovSetCorp <shilov-rom@mail.ru>
2019-02-04 16:09:26 +0300

    Encryption-Decryption

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Crypto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-	// write your code here
+        String operation = scanner.nextLine();
+        String mess = scanner.nextLine();
+        int key = scanner.nextInt();
+        StringBuilder sb = new StringBuilder(mess);
+        for (int i = 0; i < sb.length(); i++) {
+            if(operation.equals("enc")) {
+                sb.setCharAt(i, (char) (sb.charAt(i) + key));
+            }else{
+                sb.setCharAt(i, (char) (sb.charAt(i) - key));
+            }
+        }
+        System.out.println(sb);
     }
 }

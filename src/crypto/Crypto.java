8995c042a11c27e744d210eba9848f99a624fa88
ShilovSetCorp <shilov-rom@mail.ru>
2019-02-04 16:34:08 +0300

    Encryption-Decryption with command line args

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class Crypto {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String operation = scanner.nextLine();
-        String mess = scanner.nextLine();
-        int key = scanner.nextInt();
+        int key = 0;
+        String operation = "enc";
+        if(args[0].equals("-mode")){
+            operation = args[1];
+        }
+        if (args[2].equals("-key")){
+            key = Integer.parseInt(args[3]);
+        }
+        String mess = args[args.length - 1];
+
         StringBuilder sb = new StringBuilder(mess);
         for (int i = 0; i < sb.length(); i++) {
             if(operation.equals("enc")) {

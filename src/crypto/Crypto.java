27aa044098b173a45e5052d76dc9194c0d6e396e
Kirill Kornakov <KirillKornakov@narod.ru>
2019-02-05 22:43:56 +0300

    Working with files/kornakov

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,19 +1,27 @@
 package crypto;
-
 import java.util.Scanner;
-
+import java.io.*;
 public class Crypto {
-    private static void encryption(String message, int key) {
+    private static void encryption(String message, int key, String out) {
         char let;
         char let2;
         StringBuilder str = new StringBuilder(message);
         for (int i=0; i<str.length(); i++){
             let = str.charAt(i);
             let2=(char)(let+key);
-                str.setCharAt(i, let2);}
-        System.out.println(str);
+            str.setCharAt(i, let2);}
+        if (out.length()>0){
+            File file = new File(out);
+           try(FileWriter writer = new FileWriter(file)){
+               writer.write(str.toString());
+           } catch (IOException e){
+               System.out.println("The file can not be opened: " + out);
+           }
+
+        }
+        else  System.out.println(str);
     }
-    private static void decryption(String message, int key) {
+    private static void decryption(String message, int key, String out) {
         char let;
         char let2;
         StringBuilder str = new StringBuilder(message);
@@ -21,28 +29,56 @@ public class Crypto {
             let = str.charAt(i);
             let2=(char)(let-key);
             str.setCharAt(i, let2);}
-        System.out.println(str);
+        if (out.length()>0){
+            File file = new File(out);
+            try(FileWriter writer = new FileWriter(file)){
+                writer.write(str.toString());
+            } catch (IOException e){
+                System.out.println("The file can not be opened: " + out);
+            }
+
+        }
+        else  System.out.println(str);
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String message, target;
+        String message="", target;
         int key;
-        int indTarget=-1, indMessage=-1, indKey=-1;
-        for (int i=0; i<args.length-1; i++){
+        int kostil=0;
+        String out="";
+        int indTarget=-1, indMessage=-1, indKey=-1, indIn=-1, indOut=-1;
+        for (int i=0; i<args.length; i++){
             if(args[i].equals("-mode")) indTarget=i;
             if(args[i].equals("-key")) indKey=i;
-            if(args[i].equals("-data")) {indMessage=i; break;}
+            if(args[i].equals("-data")) indMessage=i;
+            if(args[i].equals("-in")) indIn=i;
+            if(args[i].equals("-out")) indOut=i;
         }
 
         if(indTarget>=0) target=args[indTarget+1];
         else target="enc";
         if (indKey>=0) key=Integer.valueOf(args[indKey+1]);
         else key = scan.nextInt();
-        if (indMessage>=0) message=args[indMessage+1];
-        else message = scan.nextLine();
-        if (target.equals("enc") || target.equals("")) encryption(message, key);
-        else decryption(message, key);
+
+         if (indIn>=0) {
+            File in = new File(args[indIn+1]);
+            try(Scanner scanner = new Scanner(in)){
+                message = scanner.nextLine();
+            } catch (FileNotFoundException e){
+                System.out.println("File is not found: " + in);
+                message=scan.nextLine();
+            }
+        }
+        if (message.length()>0) kostil++;
+        else if (indMessage >=0 ) message = args[indMessage+1];
+         else message=scan.nextLine();
+        if (indOut>=0) {
+            out =args[indOut+1];
+        }
+        System.out.println(out);
+        if (target.equals("enc") || target.equals("")) encryption(message, key, out);
+        else decryption(message, key, out);
 
     }
 }

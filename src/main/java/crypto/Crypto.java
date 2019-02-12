8a876cc33d2d3ee9c4a3c5cc5436336082c203bd
gt814 <admin@gt814.ru>
2019-02-13 02:18:30 +0300

    added command-line arguments

--- src/main/java/crypto/Crypto.java
+++ src/main/java/crypto/Crypto.java
@@ -1,30 +1,83 @@
 package crypto;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Crypto {
 
     private static final String ENCRYPTION = "enc";
     private static final String DECRYPTION = "dec";
 
+    private static final String MODE_KEY = "-mode";
+    private static final String CRYPTO_KEY = "-key";
+    private static final String DATA_KEY = "-data";
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String operation = scanner.nextLine();
-        String message = scanner.nextLine();
-        int key = Integer.parseInt(scanner.nextLine());
+
+        Map<String, String> params = parseArgs(args);
+
+        String operation;
+        if (params.get(MODE_KEY) == null) {
+            operation = ENCRYPTION;
+        } else {
+            operation = params.get(MODE_KEY);
+        }
+
+        int key;
+        if (params.get(CRYPTO_KEY) == null) {
+            System.out.println("Enter key:");
+            key = Integer.parseInt(SCANNER.nextLine());
+        } else {
+            key = Integer.parseInt(params.get(CRYPTO_KEY));
+        }
+
+        String data;
+        if (params.get(DATA_KEY) == null) {
+            System.out.println("Enter data string:");
+            data = SCANNER.nextLine();
+        } else {
+            data = params.get(DATA_KEY);
+        }
 
         String outputString;
 
         switch (operation) {
-            case ENCRYPTION: outputString = encrypt(message, key);
+            case ENCRYPTION: outputString = encrypt(data, key);
                 break;
-            case DECRYPTION: outputString = decrypt(message, key);
+            case DECRYPTION: outputString = decrypt(data, key);
                 break;
             default:
                 throw new UnsupportedOperationException("Operation '" + operation + "' is not supported.");
         }
 
         System.out.println(outputString);
+    }
+
+    private static Map<String,String> parseArgs(String[] args) {
+        Map<String, String> params = new HashMap<>();
+        params.put(MODE_KEY, null);
+        params.put(CRYPTO_KEY, null);
+        params.put(DATA_KEY, null);
+
+        for (int i = 0; i < args.length; i++) {
+
+            String key = args[i];
+
+            if (!key.startsWith("-")) {
+                throw new RuntimeException("The parameter key must contains the prefix '-'");
+            }
+
+            if (params.containsKey(key)) {
+                i++;
+                String value = (i < args.length) ? args[i] : null;
+                params.put(key, value);
+            } else {
+                throw new RuntimeException("Unsupported key");
+            }
+        }
+
+        return params;
     }
 
     public static String encrypt(String message, int key) {

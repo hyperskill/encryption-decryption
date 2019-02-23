b176fd206df9f432de9167255ff8f90275714372
gt814 <admin@gt814.ru>
2019-02-23 15:27:06 +0300

    refactoring and processing parameters

--- src/main/java/crypto/Crypto.java
+++ src/main/java/crypto/Crypto.java
@@ -7,71 +7,121 @@ public class Crypto {
     private static final String ENCRYPTION = "enc";
     private static final String DECRYPTION = "dec";
 
-    private static final String MODE_KEY = "-mode";
-    private static final String CRYPTO_KEY = "-key";
-    private static final String DATA_KEY = "-data";
+    static final String MODE_KEY = "-mode";
+    static final String CRYPTO_KEY = "-key";
+    static final String DATA_KEY = "-data";
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        final Map<String, String> params = parseArgs(args);
-        prepareParams(params);
+        try {
+            final Map<String, String> params = Crypto.parseArgs(args);
+            Crypto.prepareParams(params);
 
-        String mode = params.get(MODE_KEY);
-        String data = params.get(DATA_KEY);
-        int key = Integer.parseInt(params.get(CRYPTO_KEY));
-        String outputString;
+            String mode = params.get(MODE_KEY);
+            String data = params.get(DATA_KEY);
+            String stringKey = params.get(CRYPTO_KEY);
 
-        switch (mode) {
-            case ENCRYPTION: outputString = encrypt(data, key);
-                break;
-            case DECRYPTION: outputString = decrypt(data, key);
-                break;
-            default:
-                throw new UnsupportedOperationException("Operation '" + mode + "' is not supported.");
+            int key;
+            if(isDigit(stringKey)) {
+                key = Integer.parseInt(stringKey);
+            } else {
+                throw new RuntimeException("Encryption key must be an integer.");
+            }
+
+            String outputString;
+
+            switch (mode) {
+                case ENCRYPTION: outputString = encrypt(data, key);
+                    break;
+                case DECRYPTION: outputString = decrypt(data, key);
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Operation '" + mode + "' is not supported.");
+            }
+
+            System.out.println(outputString);
+
+        } catch (RuntimeException e) {
+            System.out.println("Operation failed. An error has occurred: " + e.getMessage());
         }
-
-        System.out.println(outputString);
     }
 
-    private static Map<String,String> parseArgs(String[] args) {
+    public static Map<String,String> parseArgs(String[] args) {
         Map<String, String> params = new HashMap<>();
         params.put(MODE_KEY, null);
         params.put(CRYPTO_KEY, null);
         params.put(DATA_KEY, null);
 
-        for (int i = 0; i < args.length; i++) {
+        int i = 0;
+        while(i < args.length) {
 
             String key = args[i];
 
-            if (!key.startsWith("-")) {
+            if (isNotKey(args[i])) {
                 throw new RuntimeException("The parameter key must contains the prefix '-'");
             }
 
             if (params.containsKey(key)) {
                 i++;
-                String value = (i < args.length) ? args[i] : null;
-                params.put(key, value);
+
+                if (i < args.length && isNotKey(args[i]) && args[i] != null) {
+                    params.put(key, args[i]);
+                } else  {
+                    throw new RuntimeException("Value of parameter " + key + " is not defined");
+                }
+
             } else {
                 throw new RuntimeException("Unsupported key");
+            }
+
+            if (isNotKey(args[i])) {
+                i++;
             }
         }
 
         return params;
     }
 
+    public static boolean isNotKey(String s) {
+        return !s.startsWith("-");
+    }
+
+    private static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
     public static void prepareParams (Map<String, String> params) {
         params.putIfAbsent(MODE_KEY, ENCRYPTION);
 
-        if (params.get(CRYPTO_KEY) == null) {
+        if (params.get(CRYPTO_KEY) == null || params.get(CRYPTO_KEY).isEmpty()) {
             System.out.println("Enter key:");
-            params.put(CRYPTO_KEY, SCANNER.nextLine());
+            String in = "";
+            while (in.isEmpty()) {
+                in = SCANNER.nextLine();
+                if (in.isEmpty()) {
+                    System.out.println("Value cannot be empty. Enter key:");
+                }
+            }
+            params.put(CRYPTO_KEY, in);
         }
 
-        if (params.get(DATA_KEY) == null) {
+        if (params.get(DATA_KEY) == null || params.get(DATA_KEY).isEmpty()) {
             System.out.println("Enter data string:");
-            params.put(DATA_KEY, SCANNER.nextLine());
+            String in = "";
+            while (in.isEmpty()) {
+                in = SCANNER.nextLine();
+                if (in.isEmpty()) {
+                    System.out.println("Value cannot be empty. Enter data string:");
+                }
+            }
+            params.put(DATA_KEY, in);
         }
     }
 
--- src/test/java/crypto/CryptoTest.java
+++ src/test/java/crypto/CryptoTest.java
@@ -2,11 +2,68 @@ package crypto;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Map;
+
+import static crypto.Crypto.*;
 
 public class CryptoTest {
 
+    private static final String MODE_VALUE = "enc";
+    private static final String CRYPTO_VALUE = "5";
+    private static final String DATA_VALUE = "Welcome to hyperskill!";
+
     @Test
-    public void encrypt() {
+    public void parseArgsSuccess() {
+        // GIVEN
+        String[] args = {MODE_KEY, MODE_VALUE, CRYPTO_KEY, CRYPTO_VALUE, DATA_KEY, DATA_VALUE};
+
+        // WHEN
+        Map<String,String> result = Crypto.parseArgs(args);
+
+        // THEN
+        Assert.assertEquals(MODE_VALUE, result.get(MODE_KEY));
+        Assert.assertEquals(CRYPTO_VALUE, result.get(CRYPTO_KEY));
+        Assert.assertEquals(DATA_VALUE, result.get(DATA_KEY));
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void parseArgsWithoutModeValue() {
+        // GIVEN
+        String[] args = {MODE_KEY, CRYPTO_KEY, CRYPTO_VALUE, DATA_KEY, DATA_VALUE};
+
+        // WHEN
+        Map<String,String> result = Crypto.parseArgs(args);
+
+        // THEN
+        // RuntimeException
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void parseArgsWithoutKey() {
+        // GIVEN
+        String[] args = {"nokey", MODE_VALUE};
+
+        // WHEN
+        Map<String,String> result = Crypto.parseArgs(args);
+
+        // THEN
+        // RuntimeException
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void parseArgsWithUnsupportedKey() {
+        // GIVEN
+        String[] args = {"-unsupportedkey", MODE_VALUE};
+
+        // WHEN
+        Map<String,String> result = Crypto.parseArgs(args);
+
+        // THEN
+        // RuntimeException
+    }
+
+    @Test
+    public void encryptSucsees() {
         // GIVEN
         final String message = "Welcome to hyperskill!";
         final int key = 5;
@@ -19,7 +76,7 @@ public class CryptoTest {
     }
 
     @Test
-    public void decrypt() {
+    public void decryptSuccess() {
         // GIVEN
         final String message = "\\jqhtrj%yt%m~ujwxpnqq&";
         final int key = 5;

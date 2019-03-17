2d9560f38ebf3f33176b218111c60aa83e48ae5a
anprs <anprs@mail.ru>
2019-03-18 02:06:31 +0700

    Stage #3: Supporting decryption

--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -3,8 +3,36 @@ package crypto;
 import java.util.Scanner;
 
 public class Crypto {
+    public static String encrypt(String text, int key) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            sb.append((char)(text.charAt(i)^key));
+            //sb.append(Character.isAlphabetic(text.charAt(i)) ? (char)((text.charAt(i)-'a'+key)%26+'a') : text.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static String decrypt(String text, int key) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            sb.append((char)(text.charAt(i)^key));
+            //sb.append(Character.isAlphabetic(text.charAt(i)) ? (char)((text.charAt(i)+'a'-key)%26-'a') : text.charAt(i));
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-	// write your code here
+        String operation = scanner.nextLine();
+        String text = scanner.nextLine();
+        int key = scanner.nextInt();
+        switch (operation) {
+            case "enc":
+                System.out.println(encrypt(text, key));
+                break;
+            case "dec":
+                System.out.println(decrypt(text, key));
+                break;
+        }
     }
 }
